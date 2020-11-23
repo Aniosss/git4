ec27d411fc5d6bf8ed40f9a51be5e2683ac23fab
Aniosss <rom.rodionov48@gmail.com>
2020-11-23 20:09:54 +0300

    new

--- main.py
+++ main.py
@@ -1,37 +1,14 @@
 import sys
-from random import randint
+import sqlite3
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
-from PyQt5.QtGui import QPainter, QColor, QPolygonF
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
 class Program(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.flag = False
         uic.loadUi('untitled.ui', self)
-        self.pushButton.clicked.connect(self.run)
 
-    def drawf(self):
-        self.flag = True
-        self.update()
-
-    def paintEvent(self, event):
-        if self.flag:
-            self.qp = QPainter()
-            self.qp.begin(self)
-            self.qp.setPen(QColor(randint(0,255), randint(0,255), randint(0,255)))
-            self.qp.setBrush(QColor(randint(0,255), randint(0,255), randint(0,255)))
-            self.draw()
-            self.qp.end()
-
-    def draw(self):
-        r = randint(10, 200)
-        coords = (randint(0, 600), randint(0, 400))
-        self.qp.drawEllipse(*coords, r, r)
-
-    def run(self):
-        self.drawf()
 
 
 if __name__ == '__main__':
