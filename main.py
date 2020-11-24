54101559d9750d4bc16f2fa5ea09282654c85f84
Aniosss <rom.rodionov48@gmail.com>
2020-11-24 16:40:04 +0300

    new

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import sys
 import sqlite3
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
 
 
 class Program(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('untitled.ui', self)
-
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        res = self.connection.cursor().execute("SELECT * FROM coffee").fetchall()
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
 
 
 if __name__ == '__main__':
